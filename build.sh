bc3412487fb66eb34cbe51ba38cca41d20feea73
nearbydelta <cd4209@gmail.com>
2018-01-13 00:52:37 +0900

    Release V1.1.1

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ twine-3 upload dist/koalanlp-*.whl
 
 VERSION = `ls dist | grep koalanlp | cut -d- -f2`
 git add docs/
+git add -i
 git commit -m "Release: v${VERSION}"
 git tag v${VERSION}
-git push --tags
+git push --all
