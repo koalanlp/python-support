9e3e01246d21355b007e449214085b642e629cef
bydelta <cd4209@gmail.com>
2018-11-30 15:42:00 +0900

    Initial commit for v2.0.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except(IOError, ImportError):
 
 setup(
     name='koalanlp',
-    version='2.0.4',
+    version='2.0.5-SNAPSHOT',
     description='Python wrapper for KoalaNLP',
     long_description=long_description,
     author='koalanlp',
