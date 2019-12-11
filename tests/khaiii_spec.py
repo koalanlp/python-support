b4fc412d5b9e19f3306bb77c97b93d8c34ff1615
bydelta <cd4209@gmail.com>
2019-12-11 12:17:53 +0900

    Add khaiii library path during test.

--- tests/khaiii_spec.py
+++ tests/khaiii_spec.py
@@ -8,6 +8,9 @@ from tests.proc_core_spec import test_Tagger_Sentence_typecheck
 
 @pytest.fixture(scope="session")
 def environ():
+    lib_path = os.environ['KHAIII_LIB']
+    os.environ['LD_LIBRARY_PATH'] = '%s:%s' % (lib_path, os.environ.get('LD_LIBRARY_PATH', ''))
+
     Util.initialize(KHAIII="LATEST")
     tagger = Tagger(API.KHAIII, kha_resource=os.environ['KHAIII_RSC'])
     yield None, tagger, None, None, None
