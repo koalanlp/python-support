97bd1ba3504d09f6e76e90cca6afb2a42ec3ee1b
bydelta <cd4209@gmail.com>
2019-12-11 15:30:27 +0900

    Add khaiii library path in travis CI build.

--- tests/khaiii_spec.py
+++ tests/khaiii_spec.py
@@ -9,9 +9,7 @@ from tests.proc_core_spec import test_Tagger_Sentence_typecheck
 @pytest.fixture(scope="session")
 def environ():
     lib_path = os.environ['KHAIII_LIB']
-    os.environ['LD_LIBRARY_PATH'] = '%s:%s' % (lib_path, os.environ.get('LD_LIBRARY_PATH', ''))
-
-    Util.initialize(KHAIII="LATEST")
+    Util.initialize(KHAIII="LATEST", java_options="-Xmx1g -Dfile.encoding=utf-8 -Djna.library.path=%s" % lib_path)
     tagger = Tagger(API.KHAIII, kha_resource=os.environ['KHAIII_RSC'])
     yield None, tagger, None, None, None
     del tagger
