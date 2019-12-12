d3cfc63924730d9e87fdcc4252c759534bac49a5
bydelta <cd4209@gmail.com>
2019-12-12 11:25:49 +0900

    Release: v2.1.1

---
+++ koalanlp/jip/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import koalanlp.jip.util
+import koalanlp.jip.maven
+import koalanlp.jip.cache
+import koalanlp.jip.index
+import koalanlp.jip.repository
--- setup.py
+++ setup.py
@@ -9,14 +9,12 @@ except(IOError, ImportError):
 
 setup(
     name='koalanlp',
-    version='2.1.1-SNAPSHOT',
+    version='2.1.1',
     description='Python wrapper for KoalaNLP',
     long_description=long_description,
     author='koalanlp',
     url='https://koalanlp.github.io/python-support',
-    install_requires=[
-        "py4j~=0.10", "jip~=0.9.13"
-    ],
+    install_requires=["py4j~=0.10", "requests~=2.22"],
     packages=find_packages(exclude=["docs", "tests", "doc_source", "scripts"]),
     keywords=['korean', 'natural language processing', 'koalanlp', '한국어 처리', '한국어 분석',
               '형태소', '의존구문', '구문구조', '개체명', '의미역'],
