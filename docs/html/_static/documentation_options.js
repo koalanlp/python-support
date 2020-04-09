cfc75b3c82685f38f65a9092f5067c1cc344d104
bydelta <cd4209@gmail.com>
2020-04-09 16:24:09 +0900

    Release: v2.1.4

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,9 +1,11 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.1.3',
+    VERSION: '2.1.4',
     LANGUAGE: 'ko',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false
